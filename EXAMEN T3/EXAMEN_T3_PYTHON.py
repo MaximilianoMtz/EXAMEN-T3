9cc20cf133468d0d4d29353aae467290e3050104
MaximilianoMtz <d23153137@aguascalientes.tecnm.mx>
2025-03-05 17:27:17 -0600

    Add files via upload

---
+++ EXAMEN T3/EXAMEN_T3_PYTHON.py
@@ -0,0 +1,10 @@
+# Solicitar al usuario que ingrese tres números decimales
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+
+# Sumar los tres números
+suma = num1 + num2 + num3
+
+# Mostrar el resultado
+print("La suma de los tres números es:", suma)
